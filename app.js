4cb79da936eb0813ba66d4d9b507fdf2ddd85056
Züleyha Kurt <netice2017@gmail.com>
2022-09-15 14:40:40 +0300

    edit

--- app.js
+++ app.js
@@ -8,28 +8,28 @@ document.querySelector(".btn").addEventListener("click",()=>{
         if(select === "Konut Kredisi"){
             faizOranı=1.29
            }
-           else if(select==='Ihtiyac Kredisi'){
+           else if(select==='İhtiyaç Kredisi'){
             faizOranı = 1.99;
            }
-            else if (select=== 'Arac Kredisi') {
+            else if (select=== 'Araç Kredisi') {
             faizOranı = 1.79;
           }
     document.querySelector("#faiz").innerHTML= faizOranı
        
     const vade= document.querySelector("#vade1").value
     console.log(vade)
-    document.querySelector("#vade").innerHTML= vade
+    document.querySelector("#vade").innerHTML= `${vade} ay`
        
     const  tutar= document.querySelector("#tutar1").value
     console.log(tutar)
-    document.querySelector("#miktar").innerHTML= tutar
+    document.querySelector("#miktar").innerHTML= `${tutar} ₺`
 
     const taksit= (tutar*((faizOranı/100)*(1+faizOranı/100)**vade))/((1+faizOranı/100)**vade-1)
     console.log(taksit)
-    document.querySelector("#taksit").innerHTML= Math.round(taksit)
+    document.querySelector("#taksit").innerHTML= `${Math.round(taksit)} ₺`
 
        const toplamTutar=taksit*vade
-       document.querySelector("#tutar").innerHTML=Math.round(toplamTutar)
+       document.querySelector("#tutar").innerHTML=`${Math.round(toplamTutar)} ₺`
 
 })
 
